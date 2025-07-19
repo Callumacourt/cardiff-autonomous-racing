f3b93fc23b274580c2f707c53ef36997c606ee49
Thomas Oldridge <OldridgeTA@cardiff.ac.uk>
2025-07-19 09:02:25 +0100

    moved logs to a better place

--- Control/start_control.sh
+++ Control/start_control.sh
@@ -1,8 +1,8 @@
 source install/setup.bash
 ./ros_can/FS-AI-API/setup.sh
-date >> ros_can_output.log 
+date >> logs/ros_can_output.log 
 ros2 launch ros_can ros_can.launch.py >> logs/ros_can_output.log 2>&1 &
 
 export PYTHONPATH="$PYTHONPATH:$(pwd)/ros_control/ros_control"
-date >> control_output.log
+date >> logs/control_output.log
 ros2 run ros_control command_node >> logs/control_output.log 2>&1 &
