2dc695b3f2b01f9be654aa94ee2533efd7d27054
Thomas Oldrige <OldridgeTA@cardiff.ac.uk>
2025-07-19 18:52:21 +0100

    autostart control

--- Control/autostart_control.sh
+++ Control/autostart_control.sh
@@ -1,8 +1,8 @@
 source ~/cardiff-autonomous-racing/Control/install/setup.bash
-./cardiff-autonomous-racing/Control/ros_can/FS-AI-API/setup.bash
+sh ~/cardiff-autonomous-racing/Control/ros_can/FS-AI-API/setup.sh
 date >> ~/cardiff-autonomous-racing/Control/logs/ros_can_output.logs
 ros2 launch ros_can ros_can.launch.py >> ~/cardiff-autonomous-racing/Control/logs/ros_can_output.logs 2>&1 &
 
-export PYTHONPATH="$PYTHONPATH:~/cardiff-autonomous-racing/Control/ros_control/ros_control"
+export PYTHONPATH="$PYTHONPATH:/home/cardiff/cardiff-autonomous-racing/Control/ros_control/ros_control"
 date >> ~/cardiff-autonomous-racing/Control/logs/control_output.log
-ros2 run ros_control control_node >> ~/cardiff-autonomous-racing/Control/logs/control_output.log
+ros2 run ros_control command_node >> ~/cardiff-autonomous-racing/Control/logs/control_output.log 2>&1 &
