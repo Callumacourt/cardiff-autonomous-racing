2c6af461d5d67cfca8ea5384535f7c838d3e55eb
Thomas Oldridge <OldridgeTA@cardiff.ac.uk>
2026-05-04 00:15:16 +0100

    added alt paths

--- Control/ros_control/ros_control/tests/MPC_Visual_Test.py
+++ Control/ros_control/ros_control/tests/MPC_Visual_Test.py
@@ -46,7 +46,8 @@ def convertStatesToList(states:list[Vehicle_State]) -> list[float]:
 def isPathGoodEnough(desiredPath, predictedPath) -> bool:
     return False
 
-path_straight = [(0,0),(1,0),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0),(8,0),(9,0)]
+#path_straight = [(0,0),(1,0),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0),(8,0),(9,0)]
+path_straight = [(0,0),(1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7),(8,8),(9,9)]
 #path_straight = [(0,0),(0,1),(0,2),(0,3),(0,4),(0,5),(0,6),(0,7),(0,8),(0,9)]
 path_slight_bend_left = []
 path_slight_bend_right = []
